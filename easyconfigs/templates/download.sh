ee7a44491b4002f2d212e2e7b79cef08a1fe9b92
Data Mover <datamover@compute-0-0.localdomain>
2022-07-14 12:24:05 -0500

    Added template download and extract scripts Better error checking in pfam download,extract scripts

--- easyconfigs/templates/download.sh
+++ easyconfigs/templates/download.sh
@@ -6,24 +6,30 @@
 #SBATCH -N 1
 #SBATCH --mail-user=datamover@igb.illinois.edu
 #SBATCH --mail-type=ALL
-#SBATCH -J pfam_download
+#SBATCH -J DATABASE_download
 #SBATCH -D /home/a-m/datamover/jobs
 #SBATCH -o %x-%j.out
 # ----------------Load Modules--------------------
 # ----------------Commands------------------------
+#
+# Replace DATABASE with name of database you are downloading
+# Replace WEBSITE with remote location of database
+#
+
+DATABASE="example"
 
 if [ -z "$1" ];
 then
-	echo "Please specify pfam version number";
+	echo "Please specify ${DATABASE} version number";
 	exit 1;
 fi
 
 VERSION=$1
-MIRROR_DIR=/private_stores/mirror/pfam/$VERSION
+MIRROR_DIR=/private_stores/mirror/${DATABASE}/${VERSION}
 
 echo "`date "+%Y-%m-%d %k:%M:%S"` Downloading Files"
 mkdir -p $MIRROR_DIR
-rsync -av rsync://ftp.ebi.ac.uk/pub/databases/Pfam/releases/Pfam${VERSION}/ $MIRROR_DIR/
+rsync -av rsync://WEBSITE/${VERSION}/ $MIRROR_DIR/
 if [ $? -eq 0 ]
 then
 	echo "`date "+%Y-%m-%d %k:%M:%S"` Downloading Files Failed"
--- easyconfigs/templates/extract.sh
+++ easyconfigs/templates/extract.sh
@@ -6,21 +6,27 @@
 #SBATCH -N 1
 #SBATCH --mail-user=datamover@igb.illinois.edu
 #SBATCH --mail-type=ALL
-#SBATCH -J pfam_extract
+#SBATCH -J DATABASE_extract
 #SBATCH -D /home/a-m/datamover/jobs
 #SBATCH -o %x-%j.out
 # ----------------Load Modules--------------------
 module load pigz/2.4-IGB-gcc-8.2.0
 # ----------------Commands------------------------
+#
+# Replace DATABASE with name of database you are downloading
+# Replace WEBSITE with remote location of database#
+#
+
+DATABSE="example"
 
 if [ -z "$1" ];
 then
-	echo "Please specify pfam version number";
+	echo "Please specify ${DATABASE} version number";
 	exit 1;
 fi
 
 VERSION=$1
-MIRROR_DIR=/private_stores/mirror/pfam/${VERSION}
+MIRROR_DIR=/private_stores/mirror/${DATABASE}/${VERSION}
 
 
 echo "`date "+%Y-%m-%d %k:%M:%S"` Extracting Files"
